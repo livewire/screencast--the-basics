b4f347740a36f271716f7ca6924b95b7e57f9598
Caleb Porzio <calebporzio@gmail.com>
2023-10-07 17:06:43 -0400

    wip

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -7,10 +7,19 @@ use Livewire\Attributes\Rule;
 
 class Todos extends Component
 {
+    public $todo = '';
+
     public $todos = [
         'Take out trash',
         'Do dishes',
     ];
+
+    public function add()
+    {
+        $this->todos[] = $this->todo;
+
+        $this->reset('todo');
+    }
 
     public function render()
     {
--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,4 +1,12 @@
 <div>
+    <form wire:submit="add">
+        <input type="text" wire:model="todo">
+
+        <span>Current todo: {{ $todo }}</span>
+
+        <button type="submit">Add</button>
+    </form>
+
     <ul>
         @foreach ($todos as $todo)
             <li>{{ $todo }}</li>
